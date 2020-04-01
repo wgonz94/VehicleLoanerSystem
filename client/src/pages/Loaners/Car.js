38a390ae65e5525242ff3594a2cfcc6342670cdf
William Gonzales <wgonz94@yahoo.com>
2020-03-31 17:40:56 -0700

    small changes to css

--- client/src/pages/Loaners/Car.js
+++ client/src/pages/Loaners/Car.js
@@ -1,34 +1,62 @@
-import React, {Fragment, useContext}from 'react';
-import {OfflotContext} from './../../utils/Context/OffLotContext'
-import './style.css'
+import React, { Fragment, useContext } from "react";
+import { OfflotContext } from "./../../utils/Context/OffLotContext";
+import "./style.css";
 
-const Car = ({ make, model, year, transmission, miles, price, vin , available, id}) => {
-    const [offlot, setOfflot] = useContext(OfflotContext);
-    const sendOfflot = e => {
-        e.preventDefault();
-        setOfflot(prevState => [...prevState, {id: id, vin: vin, make: make, model: model, year: year, customer: 'Guest', notes: ['Vehicle at Vendor'] }])
-    }
+const Car = ({
+  make,
+  model,
+  year,
+  transmission,
+  miles,
+  price,
+  vin,
+  available,
+  id
+}) => {
+  const [offlot, setOfflot] = useContext(OfflotContext);
+  const sendOfflot = e => {
+    e.preventDefault();
+    setOfflot(prevState => [
+      ...prevState,
+      {
+        id: id,
+        vin: vin,
+        make: make,
+        model: model,
+        year: year,
+        customer: "Guest",
+        notes: ["Vehicle at Vendor"]
+      }
+    ]);
+  };
 
-    const loanerCar = <Fragment>
-    <div>
-    <h3>{year} {make} {model}</h3>
-    <img alt='loanervehicle' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/4186005/300/200/m2/fpnw/wm0/silhouette-car-.jpg?1521987235&s=5b3db7d2b8d1893d6d47029cb71740db'/>
-    <section>
-        <aside>
-        <p>Transmission: {transmission}</p>
-        <p>Miles: {miles}</p>
-        <p>Price: {price}</p>
-        <p>Check for Recalls - <small>{vin}</small></p>
-        </aside>
-        <button className='readyOfflot' onClick={sendOfflot}>Use as Loaner</button>
-    </section> 
-    </div>
+  const loanerCar = (
+    <Fragment>
+      <div>
+        <h3>
+          {year} {make} {model}
+        </h3>
+        <img
+          alt="loanervehicle"
+          src="https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/4186005/300/200/m2/fpnw/wm0/silhouette-car-.jpg?1521987235&s=5b3db7d2b8d1893d6d47029cb71740db"
+        />
+        <section>
+          <aside>
+            <p>Transmission: {transmission}</p>
+            <p>Miles: {miles}</p>
+            <p>Price: {price}</p>
+            <p>
+              Check for Recalls - <small>{vin}</small>
+            </p>
+          </aside>
+          <button className="readyOfflot" onClick={sendOfflot} disabled>
+            Use as Loaner
+          </button>
+        </section>
+      </div>
     </Fragment>
-return(
-<div className='bgCars'>
-    { available ? loanerCar: null}
-</div>
-)
-}
+  );
+  return <div className="bgCars">{available ? loanerCar : null}</div>;
+};
 
-export default Car
+export default Car;
--- client/src/pages/Loaners/Off-Lot.js
+++ client/src/pages/Loaners/Off-Lot.js
@@ -5,32 +5,32 @@ import { OfflotContext } from '../../utils/Context/OffLotContext';
 
 
 function OffLot() {
-//use offlot context
-const [offlots, setOfflot] = useContext(OfflotContext)
-return(
+    //use offlot context
+    const [offlots, setOfflot] = useContext(OfflotContext)
+    return (
 
-<div className='offlot'>
-    {offlots.map(offlot => (
-    <Row>
-        <Col size='md-4'>
-    <img className='offlot-img' alt='loanervehicle' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/4186005/300/200/m2/fpnw/wm0/silhouette-car-.jpg?1521987235&s=5b3db7d2b8d1893d6d47029cb71740db'/>
-       </Col> 
-       <Col size='md-3'>
-        <p className='offlot'>Stock#:{offlot.id}</p>
-    <p className='offlot'>Loaner: {offlot.year} {offlot.make} {offlot.model}</p>
-        </Col>
-        <Col size='md-3'>
-        <p className='offlot'>Customer: {offlot.customer}</p>
-    <p className='offlot'>Notes: {offlot.notes}</p>
-        </Col>
-        <Col size='md-2'>
-            <p className='offlot'>Days Off-Lot: 8 </p>
-        </Col>
-    </Row>
-    ))}
-</div>
+        <div className='offlot'>
+            {offlots.map(offlot => (
+                <Row>
+                    <Col size='md-4'>
+                        <img className='offlot-img' alt='loanervehicle' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/4186005/300/200/m2/fpnw/wm0/silhouette-car-.jpg?1521987235&s=5b3db7d2b8d1893d6d47029cb71740db' />
+                    </Col>
+                    <Col size='md-3'>
+                        <p className='offlot'>Stock#:{offlot.id}</p>
+                        <p className='offlot'>Loaner: {offlot.year} {offlot.make} {offlot.model}</p>
+                    </Col>
+                    <Col size='md-3'>
+                        <p className='offlot'>Customer: {offlot.customer}</p>
+                        <p className='offlot'>Notes: {offlot.notes}</p>
+                    </Col>
+                    <Col size='md-2'>
+                        <p className='offlot'>Days Off-Lot: 8 </p>
+                    </Col>
+                </Row>
+            ))}
+        </div>
 
-)
+    )
 }
 
 export default OffLot;
